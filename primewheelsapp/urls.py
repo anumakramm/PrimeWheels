738b536f74bef2b548732d3b25620eb778ce912c
Anum <anumakram3@gmail.com>
2024-12-18 02:07:52 -0500

    sign up , login, logout

--- primewheelsapp/urls.py
+++ primewheelsapp/urls.py
@@ -1,11 +1,16 @@
-from django.urls import path 
+from django.urls import path
 from primewheelsapp import views
+
 app_name = 'primewheelsapp'
-urlpatterns = [ 
-    path('', views.homepage, name='homepage'), 
+
+urlpatterns = [
+    path('', views.homepage, name='homepage'),
     path('aboutus/', views.aboutus, name='aboutus'),
     path('<int:cartype_no>/', views.cardetail, name='cardetail'),
     path('team/', views.TeamMembersView.as_view(), name='teammembers'),
     path('vehicles/', views.vehicles, name='vehicles'),
     path('orderhere', views.orderhere, name='orderhere'),
-    ]
+    path('signup/', views.SignUpView.as_view(), name='signup'),
+    path('login/', views.login_here, name='login_here'),
+    path('logout/', views.logout_here, name='logout'),
+]
--- primewheelsapp/views.py
+++ primewheelsapp/views.py
@@ -5,21 +5,13 @@ from django.shortcuts import render
 from django.views import View
 from django.urls import path
 from django.views.generic import ListView
-# def homepage(request):
-#     cartype_list = CarType.objects.all().order_by('id')
-#     cars_list = Vehicle.objects.all().order_by('-car_price')
-#     response = HttpResponse()
-#     heading1 = '<p>' + 'Different Types of Cars:' + '</p>'
-#     heading2 = '<p>' + 'Different Cars:' + '</p>'
-#     # response.write(heading1)
-#     response.write(heading2)
-#     # for cartype in cartype_list:
-#     #     para = '<p>' + str(cartype.id) + ': ' + str(cartype) + '</p>'
-#     #     response.write(para)
-#     for car in cars_list[:10]:
-#         para = '<p>' + str(car.id) + ': ' + str(car) + ' - ' + str(int(car.car_price)) + '</p>'
-#         response.write(para)
-#     return response
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
 
 def homepage(request):
     cartype_list = CarType.objects.all().order_by('id')
@@ -28,29 +20,10 @@ def homepage(request):
 def aboutus(request):
     return render(request, 'primewheelsapp/aboutus.html')
 
-# def aboutus(request):
-#     response = HttpResponse()
-#     heading = '<p>' + 'About Us:' + '</p>'
-#     para = '<p>' + 'We are a car dealership company that sells a variety of cars.' + '</p>'
-#     response.write(heading)
-#     response.write(para)
-#     return response
-
 def cardetail(request, cartype_no):
     cartype = get_object_or_404(CarType, pk=cartype_no)
     cars = Vehicle.objects.filter(car_type=cartype)
     return render(request, 'primewheelsapp/cardetail.html', {'cartype': cartype, 'cars': cars})
-
-# def cardetail(request, cartype_no):
-#     cartype = get_object_or_404(CarType, pk=cartype_no)
-#     cars = Vehicle.objects.filter(car_type=cartype)
-#     response = HttpResponse()
-#     heading = '<p>' + 'Cars of ' + str(cartype) + ':' + '</p>'
-#     response.write(heading)
-#     for car in cars:
-#         para = '<p>' + str(car.id) + ': ' + str(car) + ' - ' + str(int(car.car_price)) + '</p>'
-#         response.write(para)
-#     return response
 
 def vehicles(request):
     vehicle_list = Vehicle.objects.filter(inventory__gt=0)
@@ -58,21 +31,37 @@ def vehicles(request):
 
 def orderhere(request):
     return render(request, 'primewheelsapp/order.html')
-# def teammembers(request):
-#     team_members = LabMember.objects.all().order_by('first_name')
-#     response = HttpResponse()
-#     heading = '<p>' + 'Team Members:' + '</p>'
-#     response.write(heading)
-#     for member in team_members:
-#         para = '<p>' + str(member.first_name) + ' ' + str(member.last_name) + '</p>'
-#         response.write(para)
-#     return response
 
 class TeamMembersView(ListView):
     model = LabMember
-    template_name = 'primewheelsapp/team.html'  # This should be the path within the templates folder
+    template_name = 'primewheelsapp/team.html'
     context_object_name = 'team_members'
 
     def get_queryset(self):
         return LabMember.objects.all().order_by('first_name')
 
+class SignUpView(CreateView):
+    form_class = UserCreationForm
+    template_name = 'primewheelsapp/signup.html'
+    success_url = reverse_lazy('login')
+
+def login_here(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user:
+            if user.is_active:
+                login(request, user)
+                return HttpResponseRedirect(reverse('primewheelsapp:homepage'))
+            else:
+                return HttpResponse('Your account is disabled')
+        else:
+            return HttpResponse('Login details are incorrect')
+    else:
+        return render(request, 'primewheelsapp/login_here.html')
+
+@login_required
+def logout_here(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('primewheelsapp:homepage'))
